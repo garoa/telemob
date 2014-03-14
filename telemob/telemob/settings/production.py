edb1296d97bfeba02898ad2c541e39769241e97c
Marcelo Tambalo <marcelo.zokis@gmail.com>
2014-03-14 20:22:26 -0300

    remove coisas desnecessário no settings de production

--- telemob/telemob/settings/production.py
+++ telemob/telemob/settings/production.py
@@ -1,34 +1,7 @@
 # coding: utf-8
-import ConfigParser
 import os
 
 from .base import *
-
-OPT_DJANGO_CONF_APP = '/opt/django/configs/apps/{{ project_name }}.conf'
-CONF_FILE_EXISTS = os.path.exists(OPT_DJANGO_CONF_APP)
-
-
-def read_config_file():
-    '''
-    tenta ler arquivo de configuração
-    Return: objeto do tipo ConfigParser
-    '''
-    config = ConfigParser.ConfigParser()
-    config_file = None
-    try:
-        config_file = open(OPT_DJANGO_CONF_APP)
-    except IOError as e:
-        raise Exception(u"I/O error({0}): {1} - Possível arquivo inexistente.".format(e.errno, e.strerror))
-    except:
-        raise Exception(u"Erro não esperado:", sys.exc_info()[0])
-
-    config.readfp(config_file)
-
-    return config
-
-if CONF_FILE_EXISTS:
-    config = read_config_file()
-    SECRET_KEY = config.get('APP', 'secret_key')
 
 
 DJANGO_HOME = os.path.join('/opt', 'django')
