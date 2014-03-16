594c03d3571793644c4b2d3cbe44bcbb3ed79159
Mario Idival <marioidival@gmail.com>
2014-03-16 17:44:35 -0300

    #10 remove duplicate code

--- telemob/static/js/contact.js
+++ telemob/static/js/contact.js
@@ -8,11 +8,10 @@
     },
 
     check_selected: function(){
-      var contacted_by = $('#id_contacted_by option').is(':selected'),
-          result = $('#id_result optgroup option').is(':selected'),
+      var result = $('#id_result optgroup option').is(':selected'),
           id_contacted = $('#id_contacted_by');
 
-      if ( contacted_by && result && id_contacted.val() ) {
+      if ( result && id_contacted.val() ) {
         $('.send-button').removeClass('disabled');
       } else {
         $('.send-button').addClass('disabled');
