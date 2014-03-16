042f6438569e722b14c06281fe209d0e55193938
Luciano Ramalho <luciano@ramalho.org>
2014-03-16 18:13:38 -0300

    concertando issue #10 e outros ajustes, incluindo about.html

--- telemob/main/views.py
+++ telemob/main/views.py
@@ -5,6 +5,7 @@ from django.db.models import Count
 from django.template import RequestContext
 from localflavor.br.br_states import STATE_CHOICES
 from django.shortcuts import get_object_or_404, redirect, render_to_response
+from django.contrib import messages
 
 from .forms import ContactForm
 from .models import Campaign, Politician, Contact
@@ -50,7 +51,12 @@ def report_contact(request, campaign_id, politician_id):
 
     if form.is_valid():
         form.save()
-        return redirect('/')
+        msg = ('Seu contato foi registrado, grato por participar! '
+               'Se tiver tempo, aproveite para contatar outro '
+               'parlamentar agora mesmo.')
+        messages.success(request, msg)
+        return redirect('politician_list', campaign_id=campaign.pk,
+            uf=politician.uf)
 
     return render_to_response(
         'contact_add.html', {
